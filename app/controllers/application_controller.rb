9f79970f2cc51f85ef1626417b82fbe1ae37ee65
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-03-02 14:28:22 -0500

    Deleted commented out notes to self

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    #method_override will tell our application to look for the _method key in params
     set :method_override, true
     enable :sessions
     set :session_secret, "video_game_collection"
@@ -22,10 +21,6 @@ class ApplicationController < Sinatra::Base
     end
 
     def login(email, password)
-      # Check if user actually exists
-      # If so, set the session using *** IF STATEMENT ASSIGNMENTS ***
-      # 28-33. if it returns "truthy" you should end up with a local variable called "user"
-      # 28-33. if no user is found, it will return Nil and redirect to "/login"
       user = User.find_by(:email => email)
       if user && user.authenticate(password)
         session[:email] = user.email
--- app/controllers/videogames_controller.rb
+++ app/controllers/videogames_controller.rb
@@ -1,57 +1,39 @@
 class VideogamesController < ApplicationController
 
-    #get a list of all the videogames
   get "/videogames" do
-    #get all the videogames
     @videogames = Videogame.all
-    #render the view
     erb :"videogames/index"
   end
 
-  #create an event
   get '/videogames/new' do
     erb :"videogames/new"
   end
 
   post '/videogames' do
-    #model, create a new video game
     @videogame = Videogame.create(params)
-    #response
     redirect "/videogames/#{@videogame.id}"
-    # erb :"videogames/show"
   end
 
-  #show a single videogame
   get '/videogames/:id' do
-    #model
     @videogame = Videogame.find(params[:id])
-    #response
     erb :"videogames/show"
   end
 
   get '/videogames/:id/edit' do
-    #model
     @videogame = Videogame.find(params[:id])
-    #response
     erb :"videogames/edit"
   end
 
   put '/videogames/:id' do
-    #find the instance of the videogame
     videogame = Videogame.find(params[:id])
-    #perform an update on the instance using params
-    #make sure it saves
     videogame.update(params[:videogame])
     videogame.save
-    #response
     redirect "/videogames/#{videogame.id}"
   end
 
   delete '/videogames/:id' do
-    #model
     videogame = Videogame.find(params[:id])
     videogame.destroy
-    #response
     redirect "/videogames"
   end   
 end
